919b2ffdf45c91e54a3d4e66a07a4f044b384ffc
Fierblaze <155178982+Fierblaze@users.noreply.github.com>
2024-01-31 21:21:26 +0000

    stuff

--- index.js
+++ index.js
@@ -7,6 +7,45 @@ app.use(express.json());
 
 
 
+/*
+
+ Users : username / email / password 
+ Lists : name / array(items) items -> (name/quantities) /   
+ 
+ 
+ to save locally (lists if not logged in and maybe products inserted previosly)
+
+
+ API
+
+ login
+ user update   
+ register   
+ (delete account???????)
+ 
+ import list
+ export
+ share(by id and import based on that)
+ 
+ APP
+
+ portuguese and english languages in strings
+ pages for everything
+
+
+*/
+
+
+app.post("/login", function(req,res,next){
+    console.log();
+
+
+})
+
+
+
+
+
 
 
 app.listen(8801, () => console.log("started"))

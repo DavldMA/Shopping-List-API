00d60f27985ce42b6961336fa07410836d9096f3
DavldMA <imaginaqueumghoul@gmail.com>
2024-02-06 15:57:00 +0000

    Trying some things

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.post("/list/share", async function(req, res, next) {
 
 app.get("/list/share/id/:id", async function(req, res, next) {
     console.log(req.params.id)
+    res.send(__dirname + "/public/login.html")
 })
 
 app.post("/list/add", async function(req,res,next){
